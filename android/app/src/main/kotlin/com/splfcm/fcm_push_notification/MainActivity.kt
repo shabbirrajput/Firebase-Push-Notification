fcb965f2ee0f1d17950bb5e1d53bb52092bd8c68
shabbirrajput <mohmad.shabbir@9spl.com>
2023-05-02 18:08:55 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/splfcm/fcm_push_notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.splfcm.fcm_push_notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import 'firebase_options.dart';
+
+/// Used by [NavigatorKey] of app and web
+class NavigatorKey {
+  static final navigatorKey =
+      GlobalKey<NavigatorState>(debugLabel: 'navigator');
+  static final dropDownKey = GlobalKey(debugLabel: 'dropDown');
+}
+
+/// Working example of FirebaseMessaging.
+/// Please use this in order to verify messages are working in foreground, background & terminated state.
+/// Setup your app following this guide:
+/// https://firebase.google.com/docs/cloud-messaging/flutter/client#platform-specific_setup_and_requirements):
+///
+/// Once you've completed platform specific requirements, follow these instructions:
+/// 1. Install melos tool by running `flutter pub global activate melos`.
+/// 2. Run `melos bootstrap` in FlutterFire project.
+/// 3. In your terminal, root to ./packages/firebase_messaging/firebase_messaging/example directory.
+/// 4. Run `flutterfire configure` in the example/ directory to setup your app with your Firebase project.
+/// 5. Run the app on an actual device for iOS, android is fine to run on an emulator.
+/// 6. Use the following script to send a message to your device: scripts/send-message.js. To run this script,
+///    you will need nodejs installed on your computer. Then the following:
+///     a. Download a service account key (JSON file) from your Firebase console, rename it to "google-services.json" and add to the example/scripts directory.
+///     b. Ensure your device/emulator is running, and run the FirebaseMessaging example app using `flutter run`.
+///     c. Copy the token that is printed in the console and paste it here: https://github.com/firebase/flutterfire/blob/01b4d357e1/packages/firebase_messaging/firebase_messaging/example/lib/main.dart#L32
+///     c. From your terminal, root to example/scripts directory & run `npm install`.
+///     d. Run `npm run send-message` in the example/scripts directory and your app will receive messages in any state; foreground, background, terminated.
+///  Note: Flutter API documentation for receiving messages: https://firebase.google.com/docs/cloud-messaging/flutter/receive
+///  Note: If you find your messages have stopped arriving, it is extremely likely they are being throttled by the platform. iOS in particular
+///  are aggressive with their throttling policy.
+///
+/// To verify that your messages are being received, you ought to see a notification appearon your device/emulator via the flutter_local_notifications plugin.
+/// Define a top-level named handler which background/terminated messages will
+/// call. Be sure to annotate the handler with `@pragma('vm:entry-point')` above the function declaration.
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
+}
+
+/// Create a [AndroidNotificationChannel] for heads up notifications
+late AndroidNotificationChannel channel;
+
+bool isFlutterLocalNotificationsInitialized = false;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  /// Create an Android Notification Channel.
+  ///
+  /// We use this channel in the `AndroidManifest.xml` file to override the
+  /// default FCM channel to enable heads up notifications.
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
+}
+
+void showFlutterNotification(RemoteMessage message) async {
+  NotificationDetails notificationDetails = const NotificationDetails(
+      android: AndroidNotificationDetails(
+        "_channel.id",
+        "_channel.name",
+        icon: 'ic_launcher',
+        importance: Importance.max,
+        priority: Priority.high,
+        playSound: true,
+      ),
+      iOS: DarwinNotificationDetails(presentSound: true));
+  Map<String, dynamic> data = message.data;
+  flutterLocalNotificationsPlugin.show(Random().nextInt(100), data['alert'],
+      data['message'], notificationDetails);
+}
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Set the background messaging handler early on, as a named top-level function
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  if (!kIsWeb) {
+    await setupFlutterNotifications();
+  }
+
+  runApp(MessagingExampleApp());
+}
+
+/// Entry point for the example application.
+class MessagingExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messaging Example App',
+      theme: ThemeData.dark(),
+      navigatorKey: NavigatorKey.navigatorKey,
+      routes: {
+        '/': (context) => Application(),
+      },
+    );
+  }
+}
+
+/// Renders the example application.
+class Application extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _Application();
+}
+
+class _Application extends State<Application> {
+  String? _token;
+  String? initialMessage;
+  bool _resolved = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.instance.getInitialMessage().then(
+          (value) => setState(
+            () {
+              _resolved = true;
+              initialMessage = value?.data.toString();
+            },
+          ),
+        );
+
+    FirebaseMessaging.onMessage.listen(showFlutterNotification);
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cloud Messaging'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      FirebaseMessaging.instance.getToken().then((value) {
+                        String? token = value;
+                        print('token-->$token');
+                      });
+                    },
+                    child: const Text('Get Token'),
+                  ),
+                ),
+              ),
+              //MetaCard('Message Stream', MessageList()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
